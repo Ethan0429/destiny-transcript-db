9ed6230f7e90e39ca385d4587f41689b8f2a0de9
Ethan Rickert <ethanrickert0429@gmail.com>
2023-02-21 06:34:02 -0500

    multithreading to speed up transcription process

---
+++ destiny-transcribe.go
@@ -0,0 +1,147 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"sort"
+
+	vosk "github.com/alphacep/vosk-api/go"
+	uiprogress "github.com/gosuri/uiprogress"
+)
+
+func main() {
+    // use ffmpeg to split audio into chunks of 1000 frames
+    cmd := exec.Command("ffmpeg", "-loglevel", "quiet", "-i", "audio.wav", "-f", "segment",
+        "-segment_time", "1000", "-c", "copy", "audio-%09d.wav")
+    err := cmd.Run()
+    if err != nil {
+        fmt.Println("Error executing command:", err)
+        return
+    }
+    fmt.Println("audio.wav into chunked.")
+
+    // get list of audio files
+    files, err := filepath.Glob("audio-*.wav")
+    if err != nil {
+        fmt.Println("Error getting file list:", err)
+        return
+    }
+
+    // sort the files by name
+    sort.Strings(files)
+
+    // create Vosk recognizer
+    model, err := vosk.NewModel("model")
+	if err != nil {
+        fmt.Println("Error creating model:", err)
+		return
+	} else {
+        fmt.Println("Created model")
+    }
+
+    // create a channel to receive the transcription results
+    results := make([]chan []map[string]interface{}, 0)
+
+    // process each audio file in a separate goroutine
+    uiprogress.Start()
+
+    // collect the results with goroutines and wait for them to finish
+    for i, file := range files {
+        r := make(chan []map[string]interface{})
+        results = append(results, r)
+        go func(i int, file string) {
+            segments := collectSegments(file, model)
+            r <- segments
+            close(r)
+        }(i, file)
+    }
+
+    allSegments := make([]map[string]interface{}, 0)
+    for _, r := range results {
+        segments := <-r
+        allSegments = append(allSegments, segments...)
+    }
+
+    // write the results to a file
+    f, err := os.Create("transcription.json")
+    if err != nil {
+        fmt.Println("Error creating file:", err)
+        return
+    }
+    defer f.Close()
+    enc := json.NewEncoder(f)
+    enc.SetIndent("", "  ")
+    err = enc.Encode(allSegments)
+    if err != nil {
+        fmt.Println("Error writing file:", err)
+        return
+    }
+    fmt.Println("Wrote transcription to transcription.json")
+
+
+}
+
+func collectSegments(file string, model *vosk.VoskModel) []map[string]interface{} {
+	rec, err := vosk.NewRecognizer(model, 16000)
+    if err != nil {
+        fmt.Println("Error creating recognizer:", err)
+        return nil
+    }
+	rec.SetWords(1)
+
+    f, err := os.Open(file)
+    if err != nil {
+        fmt.Println("Error opening file:", err)
+        return nil
+    }
+    defer f.Close()
+    stat, err := f.Stat()
+    size := stat.Size()
+
+    // read file in 1000 frame chunks
+    chunkSize := 1000 * 2
+    numChunks := int(size) / chunkSize
+
+    bar := uiprogress.AddBar(numChunks).AppendCompleted().PrependElapsed()
+
+    // create a slice to hold the results
+    segments := make([]map[string]interface{}, 0)
+    for i := 0; i < numChunks; i++ {
+        // read chunk
+        buf := make([]byte, chunkSize)
+        _, err := f.Read(buf)
+        if err != nil {
+            fmt.Println("Error reading file:", err)
+            return nil
+        }
+
+        // process chunk
+        if rec.AcceptWaveform(buf) != 0 {
+            result := rec.Result()
+            var data map[string]interface{}
+            err := json.Unmarshal([]byte(result), &data)
+            if err != nil {
+                fmt.Println("Error unmarshalling result:", err)
+                return nil
+            }
+            segments = append(segments, data)
+        }
+        bar.Incr()
+    }
+
+    // process the final chunk
+    finalResult := rec.FinalResult()
+    var data map[string]interface{}
+    err = json.Unmarshal([]byte(finalResult), &data)
+    if err != nil {
+        fmt.Println("Error unmarshalling final result:", err)
+        return nil
+    }
+    segments = append(segments, data)
+
+    // return segments
+    return segments
+}
